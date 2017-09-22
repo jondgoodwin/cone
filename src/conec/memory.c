e837c958f7611a56a240f1dbd8e2ae50632a1606
Jonathan Goodwin <jondgoodwin@gmail.com>
2017-09-22 13:50:11 +1000

    Add logic for simple bump-pointer memory and string allocation

--- src/conec/memory.c
+++ src/conec/memory.c
@@ -1,9 +1,103 @@
 /** Memory management
  * @file
  *
+ * The compiler's memory management is deliberately leaky for high performance.
+ * Allocation is done via bump pointer within very large arenas allocated from the heap
+ * Nothing is ever freed.
+ *
  * This source file is part of the Cone Programming Language C compiler
  * See Copyright Notice in conec.h
 */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#define MEM_ARENA_SIZE 256 * 4096 - 64
+#define STR_ARENA_SIZE 128 * 4096 - 64
+
+// Global variables for memory allocation arenas
+static void *memArenaPos = NULL;
+static size_t memArenaSize = 0;
+static void *strArenaPos = NULL;
+static size_t strArenaSize = 0;
+
+/** Allocate a structure, aligned to a 16-byte boundary */
+void *memalloc(size_t size) {
+	void *memp;
+
+	// Align to 16-byte boundary
+	size = (size + 15) & ~15;
+
+	// Return next bite out of arena, if it fits
+	if (size <= memArenaSize) {
+		memArenaSize -= size;
+		memp = memArenaPos;
+		memArenaPos = (char*)memArenaPos + size;
+		return memp;
+	}
+
+	// Return a newly allocated area, if bigger than arena can hold
+	if (size > MEM_ARENA_SIZE) {
+		memp = malloc(size);
+		if (memp==NULL) {
+			puts("Error: Out of memory");
+			exit(1);
+		}
+		return memp;
+	}
+
+	// Allocate a new Arena and return next bite out of it
+	memArenaPos = malloc(MEM_ARENA_SIZE);
+	if (memArenaPos==NULL) {
+		puts("Error: Out of memory");
+		exit(1);
+	}
+	memArenaSize = MEM_ARENA_SIZE - size;
+	memp = memArenaPos;
+	memArenaPos = (char*)memArenaPos + size;
+	return memp;
+}
+
+/** Allocate memory for a string and copy contents over, if not NULL */
+void *stralloc(char *s, size_t size) {
+	void *strp;
+
+	// Give it room for C-string null terminator
+	size += 1;
+
+	// Return next bite out of arena, if it fits
+	if (size <= strArenaSize) {
+		strArenaSize -= size;
+		strp = strArenaPos;
+		strArenaPos = (char*)strArenaPos + size;
+	}
+
+	// Return a newly allocated area, if bigger than arena can hold
+	else if (size > STR_ARENA_SIZE) {
+		strp = malloc(size);
+		if (strp==NULL) {
+			puts("Error: Out of memory");
+			exit(1);
+		}
+	}
+
+	// Allocate a new Arena and return next bite out of it
+	else {
+		strArenaPos = malloc(STR_ARENA_SIZE);
+		if (strArenaPos==NULL) {
+			puts("Error: Out of memory");
+			exit(1);
+		}
+		strArenaSize = STR_ARENA_SIZE - size;
+		strp = strArenaPos;
+		strArenaPos = (char*)strArenaPos + size;
+	}
+
+	// Copy string contents into it
+	if (s) {
+		strncpy((char*)strp, s, --size);
+		*((char*)strp+size) = '\0';
+	}
+	return strp;
+}
