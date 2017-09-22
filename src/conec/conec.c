e9402006a00a8935e2abe3a371c8431e7d85d9af
Jonathan Goodwin <jondgoodwin@gmail.com>
2017-09-22 16:37:06 +1000

    main() accepts filename and loads file into an allocated string buffer

--- src/conec/conec.c
+++ src/conec/conec.c
@@ -6,10 +6,26 @@
 */
 
 #include "conec.h"
+#include "fileio.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void main(int argv, char **argc) {
+	char *filestr;
+
+	// Output compiler name and release level
 	puts(CONE_RELEASE);
+
+	if (argv<2) {
+		puts("Specify a Cone program to compile");
+		exit(1);
+	}
+
+	filestr = fileLoad(argc[1]);
+	if (filestr) {
+		printf("I wish I knew how to compile, but here is the source!\n\n%s", filestr);
+	}
+
+	getchar();
 }
