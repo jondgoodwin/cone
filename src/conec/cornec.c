6b600fe568b5a9c5e0307fd6ea19279b37919827
Jonathan Goodwin <jondgoodwin@gmail.com>
2017-09-22 12:05:24 +1000

    Establish basic scaffolding

---
+++ src/conec/cornec.c
@@ -0,0 +1,15 @@
+/** Main program file
+ * @file
+ *
+ * This source file is part of the Cone Programming Language C compiler
+ * See Copyright Notice in conec.h
+*/
+
+#include "conec.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argv, char **argc) {
+	puts(CONE_RELEASE);
+}
---
+++ src/conec/memory.c
@@ -0,0 +1,9 @@
+/** Memory management
+ * @file
+ *
+ * This source file is part of the Cone Programming Language C compiler
+ * See Copyright Notice in conec.h
+*/
+
+#include <stdlib.h>
+
