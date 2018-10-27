7e973a874d56b142b0648a7dc53bb4ea1ce3833e
Jonathan Goodwin <jondgoodwin@gmail.com>
2018-10-27 18:57:10 +1000

    Improve OS/project supports in Congo

--- tools/congo/congo.py
+++ tools/congo/congo.py
@@ -6,43 +6,76 @@ import os
 import sys
 from subprocess import call
 
-def cone_compile(pgm, objfolder, options):
-  # Make sure 'obj' folder exists
-  if not os.path.exists(objfolder):
-    os.mkdir(objfolder)
-  call("conec " + options + " -o"+objfolder + " "+pgm)
+conehome = os.environ['CONEHOME']
 
-def cone_link():
-  linkcmd = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.14.26428\\bin\\Hostx86\\x86\\link'
-  ucrtlib = 'C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.17134.0\\ucrt\\x86\\ucrt.lib'
+# Operating system-specific configuration
+if sys.platform.startswith('linux'):
+  osname = 'linux'
+  cpu = 'x64'
+  objext = '.o'
+  exeext = ''
+elif sys.platform.startswith('win'):
+  osname = 'win'
+  cpu = 'x86'
+  objext = '.obj'
+  exeext = '.exe'
+  mstoolbin = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.14.26428\\bin\\Hostx86\\x86\\'
+  msucrtlib = 'C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.17134.0\\ucrt\\x86\\ucrt.lib'
   msvclib = '/LIBPATH:C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.14.26428\\lib\\x86\\'
-  umlib = '/LIBPATH:C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.17134.0\\um\\x86\\'
-  obj = "obj/main.obj"
+  msumlib = '/LIBPATH:C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.17134.0\\um\\x86\\'
+else:
+   print("Unsupported operating system")
+   exit(1)
+
+# Obtain project name
+sep = '\\' if osname == 'win' else '/'
+project = os.getcwd().split(sep)[-1]
+
+# Determine desired build configuration
+if len(sys.argv) > 1:
+  config = sys.argv[1]
+else:
+  config = 'release'
+
+
+def link():
+  obj = builddir + "/main.obj"
   ourlib = "../../packages/conert/release/conert.lib"
-  tolink = [linkcmd, ucrtlib, msvclib, umlib, obj, ourlib, "/LTCG"] 
+  tolink = [mstoolbin+"link", msucrtlib, msvclib, msumlib, obj, ourlib, "/LTCG", "/OUT:"+exename]
   # + "msvcrt.lib oldnames.lib kernel32.lib vcruntime.lib"
   call(tolink)
 
 
-conehome = os.environ['CONEHOME']
-#  os.path.exists(os.path.join(conehome,"conec")))
-
-# Get build target
-if len(sys.argv) > 1:
-  target = sys.argv[1]
+# Establish build directory and ensure it exists
+if config == 'release':
+  builddir = 'release'
+elif config == 'debug':
+  builddir = 'debug'
+elif config == 'web':
+  builddir = 'public'
 else:
-  target = 'pgm'
+  print("Unknown configuration: " + config)
+  exit(1)
+if not os.path.exists(builddir):
+  os.mkdir(builddir)
 
-# Call Cone compiler with appropriate options
-if target == 'pgm':
-  cone_compile("main.cone", "obj", "")
-  cone_link()
-elif target == 'debug':
-  cone_compile("main.cone", "obj", "-d --ir --asm --llvmir")
-elif target == 'web':
-  cone_compile("main.cone", "public", "--wasm")
-else:
-  print("Unknown target: "+target)
+pgm = 'main'
+exename = os.path.join(builddir, project + exeext)
+objlist = []
+
+# Compile Cone program
+if config == 'release':
+  compopt = ""
+elif config == 'debug':
+  compopt = "-d --ir --asm --llvmir"
+elif config == 'web':
+  compopt = "--wasm"
+call("conec " + compopt + " -o"+builddir + " "+pgm)
+objlist.append(os.path.join(builddir, pgm + objext))
+print(objlist)
+
+if not config == 'web':
+  link()
 
 # Compile C
 # clang-cl /c /W3 /analyze- /Gy /O2 /sdl /Zc:inline /D "_MBCS" /WX- /Zc:forScope /Gd /Oy- /Oi /MD /FC /Fa"Release\\" /EHsc /nologo /Fo"public\\" /diagnostics:classic \cone\packages\conert\main.c
